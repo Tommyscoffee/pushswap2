ff8a039409d845522ebad7eff765c9285bccbafd
Akihito Tomizawa <atomizaw@c1r30s1.42tokyo.jp>
2022-01-07 22:39:04 +0900

    bi_list.cで番兵ノードを理解した

--- bi_list.c
+++ bi_list.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-struct LinkedList_tag{
-	int						value;
-	struct	LinkedList_tag	*next;
-	struct	LinkedList_tag	*prev;
-};
-void	init_head()
+// http://www3.nit.ac.jp/~tamura/algorithm/lesson08.html　番兵ノードの説明
+// https://hiroyukichishiro.com/list-in-c-language/#i-4 
+typedef struct s_bi_list {
+	int					value;
+	t_bi_list	*next;
+	t_bi_list	*prev;
+}		t_bi_list;
+void	init_sentinel(t_bi_list *sentinel)
 {
-	gHead.value = 0;
-	gHead.next = &gHead;
-	gHead.prev = &gHead;
+	sentinel->next = sentinel;
+	sentinel->prev = sentinel;
+	sentinel->value = 0;
+	return ;
+}
+void	push_back_list(t_bi_list *sentinel, int value)
+{
+	t_bi_list	*node;
+	t_bi_list	*p;
+
+	p = sentinel;
+	node = (t_bi_list *)malloc(sizeof(t_bi_list));
+	if (!node)
+	{
+		printf("malloc error\n");
+		exit(1);
+	}
+	while (p->next != sentinel)//リストの最後まで回してあげる
+	{
+		p = p->next;
+	}
+	p->next->prev = node;//番兵ノードのprevを新しく追加するノードにしている
+	p->next = node;
+
+	node->prev = p;
+	node->value = value;
+	node->next = sentinel;//追加するノードのnextは番兵ノードになる。
 }
 
+void	show_list()
 int	main()
 {
-	init_head();
-
+	t_bi_list	*sentinel;//番兵ノードとして宣言
+	init_sentinel(sentinel);//番兵ノードの作成
+	push_back_list(sentinel, 1);
 	while( 1 ){
 		print_explain();
 
--- linked_list2.c
+++ linked_list2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// https://www.youtube.com/watch?v=b2hcyLQHQxs
 struct s_node{
 	int	data;
 	struct s_node *next;
@@ -13,10 +13,11 @@ struct s_node *make_list()
 	first = NULL;
 	struct s_node **p = &first;
 	int data;
+	int	list[] = {1,2,3,4,5,6};
 	int i = 0;
-	while (i++ < 5)
+	while (i <= 5)
 	{
-		scanf("%d\n",&data);
+		data = list[i];
 		struct s_node *node = malloc(sizeof(struct s_node) * 1);
 		if (node){
 			node->data = data;
@@ -26,46 +27,56 @@ struct s_node *make_list()
 		}
 		else
 			break;
+		i++;
 	}
 	return (first);
 };
 
-void	delete_list(struct s_node *first)
+void	delete_list(struct s_node *p)
 {
-	while (first)
+	while (p)
 	{
-		struct s_node *next = first->next;
-		free(first);
-		first = next;
+		struct s_node *next = p->next;
+		free(p);
+		p = next;
 	}
 }
 void	display(struct s_node *p)
 {
-	while (p != NULL)
+	// while (p->next != NULL)
+	// {
+	// 	printf("%d\n", p->data);
+	// 	p = p->next;
+	// }
+	while (p)
 	{
-		printf("%d\n", p->data);
+		printf("%d\n",p->data);
 		p = p->next;
 	}
+	// printf("%d\n", p->data);
 }
 int	list_delete_negatives(struct s_node **p)
 {
 	while (*p)
 	{
-	if ((*p)->data < 0)
-	{
-		struct s_node *next = (*p)->next;
-		free(*p);
-		(*p) = next;
+		if ((*p)->data < 0)
+		{
+			struct s_node *next = (*p)->next;
+			free(*p);
+			(*p) = next;
+		}
+		else
+			p = &((*p)->next);
 	}
-	else
-		p = (*p)->next;
-	}
+	return (0);
 }
 
 
 int main()
 {
 	struct s_node *first = make_list();
+	list_delete_negatives(&first);
+	display(first);
 	delete_list(first);
 	return (0);
 }
